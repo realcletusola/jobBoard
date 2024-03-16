8e60f81b4c848a5c8ce7cae1ceedc35c4910c8f7
cletusola <olamicolo@gmail.com>
2024-03-16 05:29:09 -0400

    modified settings.py

--- JobBoard/settings.py
+++ JobBoard/settings.py
@@ -173,3 +173,13 @@ SIMPLE_JWT = {
 
 #cors headers settings
 CORS_ALLOW_ALL_ORIGINS = True
+
+
+# emial backend settings 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.example.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your_email@example.com'
+EMAIL_HOST_PASSWORD = 'your_email_password'
+DEFAULT_FROM_EMAIL = 'your_email@example.com'
