28ca9812ab2aba24f64cc9fed88a556e5edabbc6
cletusola <olamicolo@gmail.com>
2024-03-16 05:30:55 -0400

    modified views.py

--- listing/views.py
+++ listing/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
-# Create your views here.
+from .models import JobListing
+from .serializers import JobListingSerializer
+from .mixins import RedisCacheMixin 
+
+
+
+# Job List View
+class JobListView(RedisCacheMixin, ListAPIView):
+    queryset = JobListing.objects.all()
+    serializer_class = JobListingSerializer
+
+
+# Job Detail View 
+class JobDetailView(RedisCacheMixin, RetrieveAPIView):
+    queryset = JobListing.objects.all()
+    serializer_class = JobListingSerializer
+
