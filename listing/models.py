c8a53fb8827c6a307b5213d2063e17073ecbfdc6
cletusola <olamicolo@gmail.com>
2024-03-16 05:30:40 -0400

    modified models.py

--- listing/models.py
+++ listing/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+# Job Listing Model
+class JobListing(models.Model):
+    title = models.CharField(max_length=30, blank=False, null=False)
+    company = models.CharField(max_length=30, blank=False, null=False)
+    description = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_posted']
+        
+    def __str__(self):
+        return self.title 
