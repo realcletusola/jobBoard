69652752cc1043e6860ea1179aed2d4fdf2ab93a
cletusola <olamicolo@gmail.com>
2024-03-20 05:24:32 -0400

    modified serializers.py

--- listing/serializers.py
+++ listing/serializers.py
@@ -8,7 +8,15 @@ class JobListingSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = JobListing
-        fields = ['id','title','company','description','date_posted','deadline']
+        fields = ['id','title','company','description','company_mail','date_posted','deadline']
 
 
-      
+
+
+# Job application serializer 
+class JobApplicationSerializer(serializers.Serializer):
+    user = serializers.CharField(required=False)
+    resume = serializers.FileField(required=True)
+    cover_letter_file = serializers.FileField(required=False)
+    cover_letter_text = serializers.TextField(required=False)
+
