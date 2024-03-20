a33f7657b09fec9fceb03b59bab04a6815c473cb
cletusola <olamicolo@gmail.com>
2024-03-20 05:24:09 -0400

    modified models.py

--- listing/models.py
+++ listing/models.py
@@ -6,6 +6,7 @@ class JobListing(models.Model):
     title = models.CharField(max_length=30, blank=False, null=False)
     company = models.CharField(max_length=30, blank=False, null=False)
     description = models.TextField()
+    company_mail = models.EmailField(max_length=60, blank=True, null=True)
     date_posted = models.DateTimeField(auto_now_add=True)
     deadline = models.DateField(auto_now_add=True)
 
